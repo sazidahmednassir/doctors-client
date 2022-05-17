9a165df06b69f9e69f6aa00c292d2897ca4d020c
sazidahmednassir <sazidahmednassir@gmail.com>
2022-05-17 10:59:32 -0800

    add imgbb api key for image

--- src/Pages/DashBoard/AddDoctor.js
+++ src/Pages/DashBoard/AddDoctor.js
@@ -8,8 +8,30 @@ const AddDoctor = () => {
 
     const {data : services, isLoading}=useQuery('services',  ()=> fetch('http://localhost:5000/service').then(res=>res.json()))
 
+    const imageStorageKey='d3283079f2b010a804b27fc40bb213a7';
+
     const onSubmit = async data => {
-      
+        const image = data.image[0];
+        const formData = new FormData();
+        formData.append('image', image);
+        const url=`https://api.imgbb.com/1/upload?key=${imageStorageKey}`
+        fetch(url,  {
+            method: 'POST',
+            body: formData
+        })
+        .then(res=>res.json())
+        .then(result =>  {
+                if(result.success){
+                    const img = result.data.url;
+                    const doctor = {
+                        name: data.name,
+                        email: data.email,
+                        specialty: data.specialty,
+                        img: img
+                    }
+                }
+                
+                console.log('image', result)})
         console.log('data', data);
        
     }
