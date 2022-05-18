99a94a505ab60cd7a317891c64996c193f5d288d
sazidahmednassir <sazidahmednassir@gmail.com>
2022-05-18 00:20:11 -0800

    add manage page full created

--- src/Pages/DashBoard/ManageDoctors.js
+++ src/Pages/DashBoard/ManageDoctors.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
+import DoctorRow from './DoctorRow';
+
 
 const ManageDoctors = () => {
+    
 
     const { data: doctors, isLoading, refetch } = useQuery('doctors', () => fetch('http://localhost:5000/doctor', {
         headers: {
@@ -13,9 +16,35 @@ const ManageDoctors = () => {
     if (isLoading) {
         return <Loading></Loading>
     }
+
     return (
         <div>
-            <h2 className="text-2xl">Manage Doctors : {doctors.length}</h2>
+            <h2 className="text-2xl">Manage Doctors: {doctors.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Specialty</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors.map((doctor, index) => <DoctorRow
+                                key={doctor._key}
+                                doctor={doctor}
+                                index={index}
+                                refetch={refetch}
+                                
+                            ></DoctorRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+          
         </div>
     );
 };
