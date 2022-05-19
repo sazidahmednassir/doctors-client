698eefa5562882715f7a032a80e8b10c1a04207a
sazidahmednassir <sazidahmednassir@gmail.com>
2022-05-19 02:17:48 -0800

    create stripe integration apid

--- src/Pages/DashBoard/Payment.js
+++ src/Pages/DashBoard/Payment.js
@@ -1,12 +1,43 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import Loading from '../Shared/Loading';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51L12G5E7oe4OQujD2P6Owg8hg7iqR6b6KDm5Aa1bNP6yWP9sXqaCRxrFZhpg3xhiqOTf95MwqtnZhc7toQl7vvws00074HQHZu');
 
 const Payment = () => {
     const {id}=useParams();
+    const url=`http://localhost:5000/booking/${id}`;
+    const {data: appointment, isLoading}=useQuery(['booking', id], ()=>fetch(url, {
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }}).then((res)=>res.json()))
+
+        if(isLoading){
+            return <Loading></Loading>
+        }
     return (
         <div>
-            <h2 className="text-2xl text-purple-500">Pay for: {id}</h2>
+        <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+            <div class="card-body">
+                <p className="text-success font-bold">Hello, {appointment.patientName}</p>
+                <h2 class="card-title">Please Pay for {appointment.treatment}</h2>
+                <p>Your Appointment: <span className='text-orange-700'>{appointment.date}</span> at {appointment.slot}</p>
+                <p>Please pay: ${appointment.price}</p>
+            </div>
         </div>
+        <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+            <div class="card-body">
+                <Elements stripe={stripePromise}>
+                    <CheckoutForm appointment={appointment} />
+                </Elements>
+            </div>
+        </div>
+    </div>
     );
 };
 
